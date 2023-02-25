fc0c92c71cab0031f41e9f329e1aadc355a17ee3
Дмитрий Екименков <sdakhj@mail.ru>
2023-02-25 17:23:35 +0300

    fix

--- products/models.py
+++ products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=250, verbose_name='наименование продукта')
-    price = models.IntegerField(verbose_name='Цена')
+    name = models.CharField(max_length=250, verbose_name='Наименование продукта')
+    price = models.IntegerField(verbose_name='Цена продукта')
     description = models.TextField(verbose_name='Описание')
     category = models.CharField(max_length=100, verbose_name='Категория')
 
@@ -15,21 +15,22 @@ class Product(models.Model):
         return f'{self.name} {self.category} {self.price} {self.description}'
 
 
-class Version:
+class Version(models.Model):
     STATUS_ACTIVE = 'active'
-    STATUS_INACTIVE = 'inacticve'
+    STATUS_INACTIVE = 'inactive'
     STATUSES = (
         (STATUS_ACTIVE, 'активна'),
-        (STATUS_INACTIVE, 'неактивна')
+        (STATUS_INACTIVE, 'неактивна'),
     )
+
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     version_number = models.IntegerField(verbose_name='Номер версии')
     version_name = models.CharField(max_length=250, verbose_name='Имя версии')
     version_status = models.CharField(choices=STATUSES, default=STATUS_INACTIVE, max_length=10, verbose_name='Статус')
 
     class Meta:
-        verbose_name = 'Версия'
-        verbose_name_plural = 'Версии'
+        verbose_name = 'версия'
+        verbose_name_plural = 'версии'
 
     def __str__(self):
-        return f'{self.product} {self.version_number} {self.version_name} {self.version_status}'
+        return f'{self.version_number} {self.version_name} {self.version_status}'
